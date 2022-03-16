09aefba29c7f6632ada92c0b59f016fd656097e8
Iskak Toltay <y.toltay@lancaster.ac.uk>
2022-03-16 17:01:03 +0000

    finished task 3 and started task 4

--- cw2/task3.py
+++ cw2/task3.py
@@ -2,13 +2,23 @@ from scipy.optimize import curve_fit
 import numpy as np
 # from dhCheck_Task3 import dhCheckCorrectness
 def Task3(x, y, num1, num2, num3, num4, bound_y):
-    def fn(xdata, a, b0, b1, b2, b3, b4):
-        return a+b0*xdata[0]+b1*xdata[1]+b2*xdata[2]+b3*xdata[3]+b4*xdata[4]
+    def fn(xdata, b0, b1, b2, b3, b4, b5):
+        return b0+b1*xdata[0]+b2*xdata[1]+b3*xdata[2]+b4*xdata[3]+b5*xdata[4]
     weights, pcov = curve_fit(fn, x, y)
-    num5 = 0
+    num5 = 10
+    funct = weights[0] + num1*weights[1] + num2*weights[2] + num3*weights[3] + num4*weights[4]
+    # ask if it has to be in x5 array, if not use commented solution
+    for x5 in x[4]:
+        if funct+x5*weights[5] >= 160:
+            if num5 > x5:
+                num5 = x5
+    # for x5 in range(11):
+    #     if funct+x5*weights[5] >= 160:
+    #         if num5 > x5:
+    #             num5 = x5
     return (weights, num5)
 
-x = np.array([[5,4,8,8,2,5,5,7,8,8], [3,7,7,2,2,5,10,4,6,3],[8,3,6,7,9,10,6,2,2,3],[8,3,6,7,9,10,6,2,2,3],[9,3,9,3,10,4,2,3,7,5],[4,9,6,6,10,3,8,8,4,6]])
+x = np.array([[5,4,8,8,2,5,5,7,8,8],[3,7,7,2,2,5,10,4,6,3],[8,3,6,7,9,10,6,2,2,3],[9,3,9,3,10,4,2,3,7,5],[4,9,6,6,10,3,8,8,4,6]])
 y = np.array([176,170,215,146,228,145,183,151,160,151])
 num1 = 5
 num2 = 6
@@ -16,4 +26,4 @@ num3 = 8
 num4 = 4
 bound_y = 160
 result = Task3(x, y, num1, num2, num3, num4, bound_y)
-print(result[0])
+print(result)
--- cw2/task4.py
+++ cw2/task4.py
@@ -2,4 +2,18 @@ from scipy.optimize import linprog
 # from dhCheck_Task4 import dhCheckCorrectness
 def Task4(c, safeguard_effect, maintenance_load, se_bound, ml_bound, x_bound, x_initial):
     # TODO
+    Aie = []
     return(x)
+
+x_initial = [3, 5, 4, 2, 1]
+safeguard_effect = [3, 5, 4, 8, 9]
+maintenance_load = [1, 2, 5, 3, 3]
+se_bound = 1000
+ml_bound = 800
+c = [11, 6, 8, 10, 9]
+bound1 = (0, 30)
+bound2 = (0, 50)
+bound3 = (0, 20)
+bound4 = (0, 45)
+bound5 = (0, 50)
+x_bound = [bound1, bound2, bound3, bound4, bound5]
