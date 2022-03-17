a12628525b96e5110b63560dbfd524e05c2d9caa
Iskak Toltay <y.toltay@lancaster.ac.uk>
2022-03-17 17:09:18 +0000

    fixed task 4 to find additional number of deployments

--- cw2/task3.py
+++ cw2/task3.py
@@ -8,14 +8,14 @@ def Task3(x, y, num1, num2, num3, num4, bound_y):
     num5 = 10
     funct = weights[0] + num1*weights[1] + num2*weights[2] + num3*weights[3] + num4*weights[4]
     # ask if it has to be in x5 array, if not use commented solution
-    for x5 in x[4]:
-        if funct+x5*weights[5] >= 160:
-            if num5 > x5:
-                num5 = x5
-    # for x5 in range(11):
+    # for x5 in x[4]:
     #     if funct+x5*weights[5] >= 160:
     #         if num5 > x5:
     #             num5 = x5
+    for x5 in range(11):
+        if funct+x5*weights[5] >= 160:
+            if num5 > x5:
+                num5 = x5
     return (weights, num5)
 
 x = np.array([[5,4,8,8,2,5,5,7,8,8],[3,7,7,2,2,5,10,4,6,3],[8,3,6,7,9,10,6,2,2,3],[9,3,9,3,10,4,2,3,7,5],[4,9,6,6,10,3,8,8,4,6]])
--- cw2/task4.py
+++ cw2/task4.py
@@ -5,10 +5,26 @@ def Task4(c, safeguard_effect, maintenance_load, se_bound, ml_bound, x_bound, x_
     # A1 = [-abs(safeguard_effect[0])]
     A1 = []
     for i in range(5):
-        A1.append(-abs(safeguard_effect[i+1]*x_initial[i]))
+        # A1.append(-abs(safeguard_effect[i+1]*x_initial[i]))
+        A1.append(-abs(safeguard_effect[i+1]))
     Aie = [A1, maintenance_load]
+    # print(Aie)
+    se_initial = safeguard_effect[0]
+    for i in range(5):
+        se_initial += safeguard_effect[i+1]*x_initial[i]
+    se_bound = se_bound - se_initial
+    ml_initial = 0
+    for i in range(5):
+        ml_initial += maintenance_load[i]*x_initial[i]
+    ml_bound = ml_bound - ml_initial
     bie = [-se_bound, ml_bound]
-    x = linprog(c, A_ub=Aie, b_ub=bie, bounds=x_bound)
+    # print(bie)
+    total = linprog(c, A_ub=Aie, b_ub=bie, bounds=x_bound)
+    x = total.x
+    for i in range(5):
+        print(x[i])
+        print(x_initial[i])
+        x[i] = x[i] - x_initial[i]
     return(x)
 
 x_initial = [3, 5, 4, 2, 1]
@@ -17,13 +33,11 @@ maintenance_load = [1, 2, 5, 3, 3]
 se_bound = 1000
 ml_bound = 800
 c = [11, 6, 8, 10, 9]
-bound1 = (0, 30)
-bound2 = (0, 50)
-bound3 = (0, 20)
-bound4 = (0, 45)
-bound5 = (0, 50)
+bound1 = (3, 30)
+bound2 = (5, 50)
+bound3 = (4, 20)
+bound4 = (2, 45)
+bound5 = (1, 50)
 x_bound = [bound1, bound2, bound3, bound4, bound5]
 result = Task4(c, safeguard_effect, maintenance_load, se_bound, ml_bound, x_bound, x_initial)
 print(result)
-
-# ASK ABOUT SAFEGUARD EFFECT!!!
