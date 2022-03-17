a3c56ed535a5e42a50bc5fe2616227bcadf38462
Iskak Toltay <y.toltay@lancaster.ac.uk>
2022-03-17 11:43:48 +0000

    done task4 to my understanding but need to ask in labs

--- cw2/task4.py
+++ cw2/task4.py
@@ -2,11 +2,17 @@ from scipy.optimize import linprog
 # from dhCheck_Task4 import dhCheckCorrectness
 def Task4(c, safeguard_effect, maintenance_load, se_bound, ml_bound, x_bound, x_initial):
     # TODO
-    Aie = []
+    # A1 = [-abs(safeguard_effect[0])]
+    A1 = []
+    for i in range(5):
+        A1.append(-abs(safeguard_effect[i+1]*x_initial[i]))
+    Aie = [A1, maintenance_load]
+    bie = [-se_bound, ml_bound]
+    x = linprog(c, A_ub=Aie, b_ub=bie, bounds=x_bound)
     return(x)
 
 x_initial = [3, 5, 4, 2, 1]
-safeguard_effect = [3, 5, 4, 8, 9]
+safeguard_effect = [6, 3, 5, 4, 8, 9]
 maintenance_load = [1, 2, 5, 3, 3]
 se_bound = 1000
 ml_bound = 800
@@ -17,3 +23,7 @@ bound3 = (0, 20)
 bound4 = (0, 45)
 bound5 = (0, 50)
 x_bound = [bound1, bound2, bound3, bound4, bound5]
+result = Task4(c, safeguard_effect, maintenance_load, se_bound, ml_bound, x_bound, x_initial)
+print(result)
+
+# ASK ABOUT SAFEGUARD EFFECT!!!
